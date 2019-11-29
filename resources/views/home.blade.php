cff85b18838f51a7625fc81398d756a0f29b908e
上野佑樹 <>
2019-11-29 09:10:02 +0800

    途中経過　
    11月28日

---
+++ resources/views/home.blade.php
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>QUESTION</title>
+</head>
+<body>
+<!-- ヘッダー -->
+
+    <header class="site-header">
+      <h2 class="header-character"> BESTTY</h2>
+    </header><br><br>
+    <div class="menu2">
+        <div class="menuWrapper">
+          <span class="menuBtn">
+          </span>
+        </div>
+        <ul id="menu">
+          <li><a href="#">風俗を探す</a></li>
+          <li><a href="#">デリヘルを探す</a></li>
+          <li><a href="#">ちんちんを探す</a></li>
+        </ul>
+      </div>
+      
+<!-- 内容 -->
+
+<diV class="">
+
+<img src="{{ asset('img/present.png') }}" alt="a image of present" class="img-present">
+    <div class="bestty-title">
+      <h4 class="bestty-b">B</h4>
+      <h4 class="bestty-e">E</h4>
+      <h4 class="bestty-s">S</h4>
+      <h4 class="bestty-t">T</h4>
+      <h4 class="bestty-t2">T</h4>
+      <h4 class="bestty-y">Y</h4>
+    </div>
+
+
+
+</diV>
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">
+</script>
+<script src="{{ asset('js/appjs.js') }}"></script>
+</body>
+</html>
