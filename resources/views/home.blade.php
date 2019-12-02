33f6ca15b6385395957d2195d274852ff9b1244f
上野佑樹 <>
2019-12-02 09:10:25 +0800

    home
    12月1日

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <title>QUESTION</title>
+<link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <title>HOME</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 </head>
 <body>
 <!-- ヘッダー -->
@@ -38,7 +41,7 @@
 
 <diV class="">
 
-<img src="{{ asset('img/present.png') }}" alt="a image of present" class="img-present">
+<img src="{{ asset('img/present.png') }}" alt="a image of present" class="d-block mx-auto present-img">
     <div class="bestty-title">
       <h4 class="bestty-b">B</h4>
       <h4 class="bestty-e">E</h4>
@@ -47,6 +50,77 @@
       <h4 class="bestty-t2">T</h4>
       <h4 class="bestty-y">Y</h4>
     </div>
+
+
+
+    <p class="text-center">センスでプレゼントを決めるサイト</p>
+    <p class="text-center">BESTTY</p>
+    <p class="text-center">プレゼントを贈る相手を男性・女性で決めよう！</p>
+
+    <div class="img text-center">
+      
+    <img src="{{ asset('img/13791.jpg') }}" alt="a image of mens" class="img-present2">
+    <img src="{{ asset('img/13790.jpg') }}" alt="a image of womens" class="img-present2">
+      
+    </div>
+
+    <div>
+    <img src="{{ asset('img/denkyu.png') }}" alt="a image of mens" class="d-block mx-auto img-present3">
+    <p class="my-4 text-center">HOW TO</p>
+    </div>
+
+    <div class="text-center">
+    <p>BESTTY</p>
+     <div class="container-fluid"> <div class="row"><div class="col-md text-color-best text-size-best-happy">BEST</div><div class="col-md text-size-best-happy">x</div><div class="col-md text-color-happy text-size-best-happy">HAPPY</div></div> </div>
+    <p class="text-color-best">BEST</p>
+    <p>なプレゼントをして、お互いに</p>
+    <p class="text-color-happy">HAPPY</p>
+    <p>になろうという</p>
+    <p>コンセプトです。</p>
+
+
+    </div>
+
+
+
+
+
+    <footer class="footer">
+  <nav>
+    <div class="sns-icon">
+        <i class="fab fa-instagram fa-2x"></i>
+        <i class="fab fa-twitter fa-2x"></i>
+        <i class="fab fa-facebook-f fa-2x"></i>
+    </div>
+    <img src="{{ asset('img/present.png') }}" alt="a image of present" class="img-present">
+    <div class="bestty-title">
+      <h4 class="bestty-b">B</h4>
+      <h4 class="bestty-e">E</h4>
+      <h4 class="bestty-s">S</h4>
+      <h4 class="bestty-t">T</h4>
+      <h4 class="bestty-t2">T</h4>
+      <h4 class="bestty-y">Y</h4>
+    </div>
+    <ul class="footer-text" style="list-style: none; padding-left: 0;">
+      <li>
+        <a href="" class="a-footer">サポート</a>
+      </li>
+      <li>
+        <a href="" class="a-footer">個人情報保護</a>
+      </li>
+      <li>
+        <a href="" class="a-footer">利用規約</a>
+      </li>
+      <li>
+        <a href="" class="a-footer">運営会社</a>
+      </li>
+      <li>
+        <a href="" class="a-footer">Nexseed</a>
+      </li>
+    </ul>
+  </nav>
+</footer>
+<small class="copyright">@besttyinc.</small>
 
 
 
