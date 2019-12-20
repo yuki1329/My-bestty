cbda13d1758b237bcf9f357ff69258affaa53320
上野佑樹 <>
2019-12-20 09:13:00 +0800

    特になし　Alert追加

--- public/js/appjs.js
+++ public/js/appjs.js
@@ -89,7 +89,8 @@ $('.menuWrapper').on('click', function(){
       var id =  $(this).attr("id");
       if (id < 9){
       var nextIdNum = parseInt(id) + 1;
-      // alert(nextIdNum);
+      // alert(id);
+      //       alert(nextIdNum);
       
       
       var position = $("#q-box-" + nextIdNum).offset().top;
